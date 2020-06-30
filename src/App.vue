399e04f69429719d1e6e59bdf210efe6ecb0fb5f
Talentedunicorn <dev@talentedunicorn.com>
2020-06-30 23:39:09 +0800

    added GA tracking

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main id="app" :style="containerStyles">
-    <h1 class="logo" :title="appName" @click="clear">
+    <h1 class="logo" @click="clear">
       <span class="hidden">ColorMeHex</span>
     </h1>
     <form @submit.prevent>
@@ -32,7 +32,6 @@
 import invert from "invert-color";
 
 export default {
-  name: "App",
   data() {
     return {
       name: "",
