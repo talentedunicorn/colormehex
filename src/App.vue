5ee8f7b9221a3e092a7fe9ce8713737e5fb7acbd
Talentedunicorn <dev@talentedunicorn.com>
2020-06-30 22:35:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,169 @@
+<template>
+  <main id="app" :style="containerStyles">
+    <h1 class="logo" :title="appName" @click="clear">
+      <span class="hidden">ColorMeHex</span>
+    </h1>
+    <form @submit.prevent>
+      <label>
+        <span class="hidden">Type a name</span>
+        <input
+          placeholder="Type something here..."
+          type="text"
+          v-model="name"
+          ref="input"
+        />
+      </label>
+
+      <label v-if="name" v-show="hexName">
+        Your color is
+        <output @click="copyHex">{{ hexName }}</output>
+      </label>
+
+      <span v-if="copied">Copied to clipboard</span>
+    </form>
+
+    <footer>
+      <p>Made by <a href="http://talentedunicorn.com">Talentedunicorn</a></p>
+    </footer>
+  </main>
+</template>
+
+<script>
+import invert from "invert-color";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "",
+      copied: false
+    };
+  },
+  computed: {
+    hexName() {
+      return `#${this.intToRGB(this.hashCode(this.name.toLowerCase()))}`;
+    },
+    containerStyles() {
+      return {
+        "--background": this.hexName,
+        "--color": this.invertedHex
+      };
+    },
+    invertedHex() {
+      return invert(this.hexName);
+    }
+  },
+  methods: {
+    hashCode(str) {
+      let hash = 0;
+      for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+      }
+      return hash;
+    },
+    intToRGB(i) {
+      let c = (i & 0x00ffffff).toString(16).toUpperCase();
+      return "00000".substring(0, 6 - c.length) + c;
+    },
+    copyHex() {
+      navigator.clipboard.writeText(this.hexName).then(() => {
+        this.copied = true;
+        setTimeout(() => {
+          this.copied = false;
+        }, 2000);
+      });
+    },
+    clear() {
+      return (this.name = "");
+    }
+  },
+  mounted() {
+    this.$refs.input.focus();
+  }
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Share+Tech+Mono&display=swap");
+
+body {
+  margin: 0;
+}
+
+.hidden {
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+}
+
+#app {
+  --logo-color: var(--color);
+  --logo-size: 3rem;
+  --font: "Share Tech Mono", Helvetica, Arial, sans-serif;
+  --input-shadow: 0 1px 2px var(--logo-color), 1px -1px 4px var(--logo-color);
+
+  font-family: var(--font);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: var(--background);
+  color: var(--color);
+  min-height: 100vh;
+  display: flex;
+  flex-flow: column;
+  transition: 0.3s cubic-bezier(0.79, 0.14, 0.15, 0.86);
+}
+
+.logo {
+  align-self: flex-end;
+  margin: 2rem 2rem 4rem;
+  padding: calc(var(--logo-size) / 2);
+  width: 0px;
+  height: 0px;
+  mask: url("./assets/logo.svg") no-repeat center center/contain;
+  background: var(--logo-color);
+}
+
+form {
+  padding: 0 2rem;
+
+  input {
+    margin: 0 0 2rem;
+    padding: 0.5em;
+    font-size: var(--font-size, 1.2rem);
+    font-family: inherit;
+    border: none;
+    border-radius: 0.25em;
+
+    @media screen and (min-width: 40rem) {
+      --font-size: 2rem;
+    }
+  }
+
+  label {
+    display: flex;
+    flex-flow: column;
+  }
+
+  output {
+    margin: 1rem 0;
+    font-size: 3rem;
+    font-weight: bold;
+  }
+}
+
+footer {
+  margin-top: auto;
+  padding: 2rem;
+
+  a {
+    color: inherit;
+    font-weight: bold;
+    text-decoration: none;
+  }
+}
+</style>
---
+++ tests/unit/app.spec.js
@@ -0,0 +1,48 @@
+import { shallowMount } from "@vue/test-utils";
+import App from "@/App";
+
+jest.useFakeTimers();
+
+describe("App.vue", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(App);
+  });
+
+  it("should render initially correctly", () => {
+    expect(wrapper.vm.name).toBe("");
+  });
+
+  it("should render the hexName correctly", async () => {
+    wrapper.setData({ name: "Cedric" });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.hexName).toBe("#AE398A");
+  });
+
+  it("should be able to copy hex", async () => {
+    const mockedWriteText = jest.fn().mockResolvedValue(true);
+    // Mock window navigation
+    delete window.navigator;
+    window.navigator = {
+      clipboard: {
+        writeText: mockedWriteText
+      }
+    };
+
+    wrapper.setData({ name: "lucas" });
+    await wrapper.vm.$nextTick();
+    wrapper.vm.copyHex();
+    await wrapper.vm.$nextTick();
+    expect(mockedWriteText).toHaveBeenCalledTimes(1);
+    expect(mockedWriteText).toHaveBeenCalledWith(wrapper.vm.hexName);
+    jest.runAllTimers();
+    expect(wrapper.vm.copied).toBe(false);
+  });
+
+  it("should be able to clear input", () => {
+    wrapper.setData({ name: "test" });
+    wrapper.vm.clear();
+    expect(wrapper.vm.name).toBe("");
+  });
+});
